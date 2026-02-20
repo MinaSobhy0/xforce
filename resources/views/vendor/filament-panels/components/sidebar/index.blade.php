5280267e98a26380c2f5452ea72006b989c5017e
root <root@mail.xsquare-eg.com>
2026-02-20 00:29:46 +0000

    Revert "Add simple single-column sidebar for tenant admin panel"
    This reverts commit 915d69b13f51fbf23ae913dc0cd398e694b302cc.

--- resources/views/vendor/filament-panels/components/sidebar/index.blade.php
+++ resources/views/vendor/filament-panels/components/sidebar/index.blade.php
@@ -5,14 +5,9 @@
 @php
     $openSidebarClasses = 'fi-sidebar-open translate-x-0 shadow-xl ring-1 ring-gray-950/5 dark:ring-white/10 rtl:-translate-x-0';
     $isRtl = __('filament-panels::layout.direction') === 'rtl';
-    $currentPanel = filament()->getCurrentPanel()?->getId();
 @endphp
 
-{{-- Simple sidebar for admin (tenant) panel --}}
-@if($currentPanel === 'admin')
-    @include('filament.admin.components.sidebar', ['navigation' => $navigation])
-@else
-{{-- Double sidebar for super-admin panel --}}
+{{-- format-ignore-start --}}
 <aside
     x-data="{
         activeGroup: null,
@@ -190,5 +185,4 @@
         </div>
     </div>
 </aside>
-@endif
 {{-- format-ignore-end --}}
